b5281c7e6550ffd8cdf2b3417bed56e6ae2de30b
Brieden <oreja@posteo.de>
2018-02-18 23:53:13 +0100

    rethink the directory workround

--- src/kaLB.py
+++ src/kaLB.py
@@ -7,6 +7,35 @@ import json
 import argparse
 from src.d2q9_simulation import Simulation
 from src import utilities
+import os
+
+
+def parse_arguments():
+    """
+    Parse commandline arguments.
+
+    :return: args
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-i', '--input', required=True, type=str,
+        help="Specify path to input file."
+    )
+    parser.add_argument(
+        '-o', '--output', required=False, type=str,
+        default='./output/',
+        help="Specify path where to save the output."
+    )
+    parser.add_argument(
+        '-v', '--verbose', required=False, action='store_true',
+        help="With this command, you can activate an issue of many hints."
+    )
+    parser.add_argument(
+        '-s', '--snapshot', required=False, type=str,
+        help="Specify path to the existing snapshot that you want to use as initial condition."
+    )
+
+    return parser.parse_args()
 
 
 def open_json(filename):
@@ -21,40 +50,8 @@ def open_json(filename):
         input_data = json.load(open(filename))
     except IOError as error:
         print("ERROR: could not open input-file. " + str(error))
+        quit()
     return input_data
-
-
-def parse_arguments():
-    """
-    Parse commandline arguments.
-
-    :return: args
-    """
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        '-i', '--input', required=False, type=str,
-        default="./../examples/kaLB_example.json",
-        help="Specify path to input file."
-    )
-    parser.add_argument(
-        '-o', '--output', required=False, type=str,
-        default='./output/',
-        help="Specify path where to save the output."
-    )
-    parser.add_argument(
-        '-v', '--verbose', required=False, action='store_true',
-        help="With this command, you can activate an issue of many hints."
-    )
-    parser.add_argument(
-        '-t', '--test', required=False, action='store_true',
-        help="With this command, you can start a systemtest"
-    )
-    parser.add_argument(
-        '-s', '--snapshot', required=False, type=str,
-        help="Specify path to the existing snapshot that you want to use as initial condition."
-    )
-
-    return parser.parse_args()
 
 
 def main():
@@ -62,15 +59,15 @@ def main():
     Function
     """
     args = parse_arguments()
-    if args.test:
-        args.input = "./../examples/system_test.json"
-
+    do_systemtest = False
+    if args.input == "system_test.json":
+        do_systemtest = True
     input_file = args.input
     json_file = open_json(input_file)
 
     sim = Simulation(inputfile=json_file, args=args)
     sim.run_simulation()
-    if args.test:
+    if do_systemtest:
         utilities.system_test(args)
 
 
