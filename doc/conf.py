14bb90db973f317b847fa7a5c7d83b1c25ebc525
Brieden <oreja@posteo.de>
2018-02-19 06:03:20 +0100

    add favicon

--- doc/conf.py
+++ doc/conf.py
@@ -85,7 +85,7 @@ todo_include_todos = False
 # a list of builtin themes.
 # 'alabaster'
 html_theme = 'default'
-
+html_favicon = 'favicon.gif'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
